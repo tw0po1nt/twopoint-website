bde2d0bdfdd4495b3875d663950abc43c673ec29
Matthew Watt <matt@twopoint.dev>
2026-01-31 14:43:37 -0600

    Fix some line endings

--- backend/Http/Program.fs
+++ backend/Http/Program.fs
@@ -19,18 +19,18 @@ open System.Text.Json
 
 [<EntryPoint>]
 let main args =
-  let builder = FunctionsApplication.CreateBuilder(args)
-    
+  let builder = FunctionsApplication.CreateBuilder args
+
   builder.ConfigureFunctionsWebApplication() |> ignore
-  
+
   if builder.Environment.IsDevelopment() then
-    builder.Configuration.AddJsonFile("local.settings.json") |> ignore
-  
-  builder.Services.AddMicrosoftIdentityWebApiAuthentication(builder.Configuration) |> ignore
+    builder.Configuration.AddJsonFile "local.settings.json" |> ignore
+
+  builder.Services.AddMicrosoftIdentityWebApiAuthentication builder.Configuration |> ignore
   builder.Services.AddAuthorization() |> ignore
-  
+
   builder.UseMiddleware<AuthenticationMiddleware>() |> ignore
-  
+
   builder.Services.AddHttpClient() |> ignore
   builder.Services.Configure<JsonSerializerOptions>(
     Action<JsonSerializerOptions>(fun options ->
@@ -38,34 +38,34 @@ let main args =
       options.PropertyNameCaseInsensitive <- true
     )
   ) |> ignore
-  
+
   builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights()
     |> ignore
-    
+
   let config = builder.Configuration |> Config.bind |> BindResult.getOrFail
-  
+
   // SETUP DEPENDENCIES
-  
+
   // Azure credential
   let opts = DefaultAzureCredentialOptions()
   opts.TenantId <- config.Azure.EntraTenantId.ToString()
-  
+
   if not (builder.Environment.IsDevelopment())
   then opts.ManagedIdentityClientId <- config.Azure.ManagedIdentityClientId.ToString()
-  let credential = DefaultAzureCredential(opts)
-  
+  let credential = DefaultAzureCredential opts
+
   // Azure Table Storage
   let tableServiceClient = TableServiceClient(config.Azure.TableStorageUri, credential)
-  
+
   // Email client
   let resourceEndpoint = config.Azure.CommsResourceEndpoint
   let emailClient = EmailClient(resourceEndpoint,  credential)
-  
+
   // Service Bus client
   let serviceBusClient = ServiceBusClient(config.Azure.ServiceBusUri.Host, credential)
-  
+
   // Firebase Admin SDK
   FirebaseInitializer.initialize config (Some credential)
   |> Async.AwaitTask
@@ -77,7 +77,7 @@ let main args =
   builder.Services.AddScoped<TableServiceClient>(fun _ -> tableServiceClient) |> ignore
   builder.Services.AddScoped<ServiceBusClient>(fun _ -> serviceBusClient) |> ignore
   builder.Services.AddSingleton<FirebaseMessaging>(fun _ -> FirebaseMessaging.DefaultInstance) |> ignore
-    
+
   builder.Build().Run()
-  
+
   0
